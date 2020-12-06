188d93a56ed6c4131f2b5bd5d7872f2aeedd2b0f
Your Name <rosca.bianca@yahoo.com>
2020-12-06 22:46:50 +0200

    dashboard added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,32 @@
+//hide dashboard when the app starts
+const dashboard = document.querySelector('.dashboard');
+dashboard.style.display = 'none';
+const submitBtn = document.querySelector('.submit .first-btn');
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let user = new Object();
+
+const saveUserToLS = () => {
+    localStorage.setItem('user',JSON.stringify(user));
+}
+
+const getUserfromLS = () => {
+    return localStorage.getItem('user');
+} 
+ 
+
 const switchToMainWindow = () => {
-    const firstSection = document.querySelector('.set-amount');
+    const firstSection = document.querySelector('.start-app');
     firstSection.style.display = 'none';
-    console.log(user);
+    dashboard.style.display='block';
+    const body = document.querySelector('body');
+    body.style.display= 'block';
 }
 
 const switchToSecondWindow = () => {
     const h1 = document.querySelector('.set-amount h1');
-    h1.innerHTML = 'Set your default glass capacity (in liters)';
+    let copy = h1.cloneNode(true);
+    h1.parentNode.replaceChild(copy,h1);
+    copy.innerHTML = 'Set your default glass capacity (in liters)';
 
 }
 
@@ -18,15 +38,47 @@ const submitDailyAmount = () => {
         }
     else{
         user.glassCapacity = parseFloat(input.value);
+        saveUserToLS();
         switchToMainWindow();
     }
 
     input.value= '';
     submitBtn.classList.remove('first-btn');
-    // submitBtn.classList.add('second-btn');
+
 
 }
 
-const submitBtn = document.querySelector('.submit .first-btn');
-submitBtn.addEventListener('click', submitDailyAmount);
-let user = new Object();
+if (!getUserfromLS())
+{
+    submitBtn.addEventListener('click', submitDailyAmount);
+    
+}
+//go straight to dashboard if user already exists in LS
+else{
+    switchToMainWindow();
+}
+
+const getSuffix = (day) => {
+    if(day % 10 === 1)
+        return 'st';
+    else if (day % 10 ===2 )
+        return 'nd';
+    else if (day % 10 ===3)
+        return 'rd';
+    else return 'th';
+}
+
+const writeTodayDate = () =>{
+    const dateEl = document.querySelector('.date-container');
+    const date = new Date();
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    
+   
+    //update DOM
+    dateEl.innerHTML+= `${day}<sup>${getSuffix(day)}</sup> ${month} ${year}`;
+
+}
+
+writeTodayDate();
